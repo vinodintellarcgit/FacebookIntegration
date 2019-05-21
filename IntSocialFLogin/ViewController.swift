8e1c080cb716e60534220b6418f93833651d94ce
Vinod Tiwari <vinodtiwari@vinods-macbook-pro.local>
2019-05-21 10:25:17 +0530

    Initial Commit

---
+++ IntSocialFLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IntSocialFLogin
+//
+//  Created by Vinod Tiwari on 21/05/19.
+//  Copyright © 2019 Intellarc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
